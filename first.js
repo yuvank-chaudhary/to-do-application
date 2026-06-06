874f4b4e1fc74e06bcb18da6a2aeb4993d59d1d0
yuvank chaudhary <yuvankchoudhary@gmail.com>
2026-06-06 12:42:33 +0530

    adding clock to the project

--- first.js
+++ first.js
@@ -1,1 +1,22 @@
-//alert("hello world")
+let hr= document.getElementById("hrs");
+let mint= document.getElementById("mint");
+let sec= document.getElementById("sec");
+
+function displayTime(){
+    let date = new Date();
+
+    // getting hours,minutes,second from date
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    let hRotation = (30*hh + mm/2);
+    let mRotation = (6*mm);
+    let sRotation = (6*ss);
+
+    hr.style.transform = `rotate(${hRotation}deg)`;
+    mint.style.transform = `rotate(${mRotation}deg)`;
+    sec.style.transform = `rotate(${sRotation}deg)`;
+};
+
+setInterval(displayTime,1000);
